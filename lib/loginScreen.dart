ec6d35a4dd046a6d0506ff640ee57958ff031a14
Dharmik Dalwadi <dharmik.dalwadi@solutionanalysts.com>
2019-08-21 10:31:03 +0530

    Initial Started with loginScreen

---
+++ lib/loginScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(LoginScreen());
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
