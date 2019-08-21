2b2fd12ce7914125b696bd9f5f96d113291ab48e
Dharmik Dalwadi <dharmik.dalwadi@solutionanalysts.com>
2019-08-21 17:45:26 +0530

    Intia lLogin

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(LoginScreen());
+void main() => runApp(Login());
 
-class LoginScreen extends StatefulWidget {
+class Login extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _LoginState createState() => _LoginState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: Container(
+                margin: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+                child: Text(
+                  'LOG IN',
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w400),
+                ),
+              ),
+            ),
+            Text(
+              'Good to see you again',
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_my_recipes_01000101/loginScreen.dart';
+
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => Login(),
+      },
     );
   }
 }
+      
