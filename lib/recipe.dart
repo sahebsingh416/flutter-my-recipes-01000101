228b7184df1643a5eebe8531fc9e9e697f4ac98c
Saheb Singh <sahebsingh.tuteja.sa@gmail.com>
2019-08-23 03:19:35 +0530

    RecipeList Screen Added

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -13,6 +13,7 @@ class _RecipeState extends State<Recipe> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black12,
         appBar: AppBar(
           title: Text(
             "Recipes",
@@ -27,7 +28,7 @@ class _RecipeState extends State<Recipe> {
           child: ListView.builder(
             shrinkWrap: true,
             scrollDirection: Axis.vertical,
-            itemCount: 5,
+            itemCount: 6,
             itemBuilder: (BuildContext context, int index) {
               return Card(
                   margin: EdgeInsets.all(10),
@@ -44,12 +45,95 @@ class _RecipeState extends State<Recipe> {
                       Container(
                         alignment: Alignment(-0.9, 0.0),
                         margin: EdgeInsets.only(top: 10),
-                        child: Text("Soup",textAlign: TextAlign.left,style: TextStyle(color: Colors.grey),),
+                        child: Text(
+                          "Soup",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.grey),
+                        ),
                       ),
                       Container(
                         alignment: Alignment(-0.9, 0.0),
                         margin: EdgeInsets.only(top: 10),
-                        child: Text("AlfredoSauce",textAlign: TextAlign.left,style: TextStyle(fontSize: 17),),
+                        child: Text(
+                          "Alfredo Sauce",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 17),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20, bottom: 20),
+                        child: Row(
+                          children: <Widget>[
+                            Row(children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.only(left: 30),
+                              ),
+                              Container(
+                                  width: 100,
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(Icons.access_time,color: Colors.grey,),
+                                      Padding(
+                                        padding: EdgeInsets.only(left: 5),
+                                      ),
+                                      Text(
+                                        "25 Minutes",
+                                        textAlign: TextAlign.left,
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ],
+                                  ))
+                            ]),
+                            Row(children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.only(left: 15, right: 25),
+                              ),
+                              Container(
+                                  width: 80,
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(Icons.view_list,color: Colors.grey,),
+                                      Padding(
+                                        padding: EdgeInsets.only(left: 5),
+                                      ),
+                                      Text(
+                                        "Easy",
+                                        textAlign: TextAlign.left,
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ],
+                                  ))
+                            ]),
+                            Row(children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.only(left: 25),
+                              ),
+                              Container(
+                                  child: Row(
+                                children: <Widget>[
+                                  Icon(Icons.restaurant,color: Colors.grey,),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 5),
+                                  ),
+                                  Text(
+                                    "4 People",
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ))
+                            ]),
+                          ],
+                        ),
                       ),
                     ],
                   ));
